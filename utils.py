bec97aa5700fcfc9b1e50e09ad48e06bacb8965c
Lory LETICEE <loryleticee@gmail.com>
2020-05-01 05:14:01 +0200

    avoid repeat current date for print

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from moon import moon_phase
 dicto = {}
 lst = []
 tops = {}
+current_date = ''
 
 day_name = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
 month_name = ['null_month','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
@@ -13,9 +14,6 @@ month_name = ['null_month','January', 'February', 'March', 'April', 'May', 'June
 #----------------------------------------------------------------------------
 
 def ffff(sDate, sDay_phase, sTirage):
-    getMoon(sDate)
-
-
     global tops
     for number in aFinal:
         if (tops.get("{}".format(number)) is None):
@@ -58,7 +56,8 @@ def daySort(day_name, day, dayRef, aFinal, aRefValue):
 
 def topNumbers(sDate, sDay_phase, sTirage):
     global month_name
-
+    global current_date
+    
     #Stat pasted
     jour = int(sDate[0:2])
     mois = int(sDate[3:5])
@@ -79,25 +78,27 @@ def topNumbers(sDate, sDay_phase, sTirage):
     month_word = month_name[mois]
     sToday_month_word = month_name[sToday_mois]
 
-    if(str(day_word) == str(sToday_day_word)):
-        if(month_word == sToday_month_word):
-            if(status == sToday_status):
-                if(light == sToday_light):
-                    if(jour == sToday_jour):
-                        print("{}--{} {} ☀️ {}%    Today is : {} {} {} ☀️ {}% ".format(status, day_word, date,  light, sToday_status, sToday_day_word, sToday_date, sToday_light ))
-                        print("\x1b[6;30;42m' +Top 🔥🔥🔥 {} + '\x1b[0m' \n".format(sTirage))
+    if(current_date != sDate):
+        if(str(day_word) == str(sToday_day_word)):
+            if(month_word == sToday_month_word):
+                if(status == sToday_status):
+                    if(light == sToday_light):
+                        if(jour == sToday_jour):
+                            print("{}--{} {} ☀️ {}%    Today is : {} {} {} ☀️ {}% ".format(status, day_word, date,  light, sToday_status, sToday_day_word, sToday_date, sToday_light ))
+                            print("\x1b[6;30;42m' +Top 🔥🔥🔥 {} + '\x1b[0m' \n".format(sTirage))
+                        else:
+                            print("{}--{} {} ☀️ {}%    Today is : {} {} {} ☀️ {}% ".format(status, day_word, date,  light, sToday_status, sToday_day_word, sToday_date, sToday_light ))
+                            print("\x1b[6;30;42m' +Top 1 {} + '\x1b[0m' \n".format(sTirage))
                     else:
-                        print("{}--{} {} ☀️ {}%    Today is : {} {} {} ☀️ {}% ".format(status, day_word, date,  light, sToday_status, sToday_day_word, sToday_date, sToday_light ))
-                        print("\x1b[6;30;42m' +Top 1 {} + '\x1b[0m' \n".format(sTirage))
-                else:
-                    print("{}--{} {} ☀️ {}%    Today is : {} {} {} ☀️ {}% ".format(status, day_word, date,  light, sToday_status, sToday_day_word, sToday_date, sToday_light))
-                    print('Top 2 {}  \n'.format(sTirage))
+                        print("{}--{} {} ☀️ {}%    Today is : {} {} {} ☀️ {}% ".format(status, day_word, date,  light, sToday_status, sToday_day_word, sToday_date, sToday_light))
+                        print('Top 2 {}  \n'.format(sTirage))
+                #else:
+                    #print("{}--{} {} ☀️ {}%    Today is : {} {} {} ☀️ {}% ".format(status, day_word, date,  light, sToday_status, sToday_day_word, sToday_date, sToday_light))
+                    #print('Top 3 {}  \n'.format(sTirage))
             #else:
                 #print("{}--{} {} ☀️ {}%    Today is : {} {} {} ☀️ {}% ".format(status, day_word, date,  light, sToday_status, sToday_day_word, sToday_date, sToday_light))
-                #print('Top 3 {}  \n'.format(sTirage))
-        #else:
-            #print("{}--{} {} ☀️ {}%    Today is : {} {} {} ☀️ {}% ".format(status, day_word, date,  light, sToday_status, sToday_day_word, sToday_date, sToday_light))
-            #print('Top 4 {}  \n'.format(sTirage)) 
+                #print('Top 4 {}  \n'.format(sTirage)) 
+    current_date = sDate
 
 def delOldFiles():
     #Supprime tous les anciens fichiers de log
