8ca7fcaf4336f7cce2c71058b07503e16fd7c68c
Lory LETICEE <loryleticee@gmail.com>
2020-06-23 11:47:59 +0200

    clean code , add comprehension...

---
+++ constant.py
@@ -0,0 +1,27 @@
+# This Python file uses the following encoding: utf-8
+
+nbr_tirage = 60 #(997)
+
+#Chemin où sont placées les fichiers csv
+path = './keno'
+
+#12/01/2019 <--> 30/04/2020 ####### novembre 2019 à maintenant (Retelecharger le ficher à chaque nouveau tirage)
+nov19ToNow = '/keno_201811.csv'
+
+#24/02/2013 <--> 11/11/2018
+fev19ToNov10 = '/keno_gagnant_a_vie.csv'
+
+#16/09/1993 <--> 23/02/2013  
+mar17ToFev19 = '/keno.csv'
+
+# Les 3 derniers fichiers csv ne sont pas formatés de la meme façon , la colonne des resultats est L au lieu de M
+csv_files = [nov19ToNow, fev19ToNov10, mar17ToFev19]#, may76ToOct08]
+
+lap_more = count_day = 0
+lap = 1
+limit_tirages = 12
+
+tops_soir_month = {str(x): 0 for x in range(1,71)}
+tops_midi_month = {str(x): 0 for x in range(1,71)}
+tops_soir= {str(x): 0 for x in range(1,71)}
+tops_midi= {str(x): 0 for x in range(1,71)}
--- keno.py
+++ keno.py
@@ -1,116 +1,80 @@
 # This Python file uses the following encoding: utf-8
 import os, sys
 import csv
-from datetime import datetime,timedelta,date
+from datetime import datetime, timedelta, date
 from utils import topNumbers
 from match import somByDay,somByMonth, tops_midi, tops_soir, tops_midi_month, tops_soir_month
+from constant import nbr_tirage, path, nov19ToNow, fev19ToNov10, mar17ToFev19, csv_files, lap_more, count_day, lap, limit_tirages
 
-nbr_tirage = 60 #(997)
+def init():
+    #nettoie le terminal
+    os.system('clear')
 
-##Chemin où sont placées les fichiers csv
-path = './keno'
+    print('\n Mise à jour des derniers tirages en cours ...\n')
 
-#12/01/2019 <--> 30/04/2020 ####### novembre 2019 à maintenant (Retelecharger le ficher à chaque nouveau tirage)
-nov19ToNow = '/keno_201811.csv'
+    #download last result file and unzip
+    os.system('wget https://media.fdj.fr/static/csv/keno/keno_201811.zip && unzip -o keno_201811.zip')
+    #delete zip file
+    os.system('rm -rf keno_201811.zip')
+    #Move to keno file 
+    os.system('mv keno_201811.csv keno')
+    print('Done')
 
-#24/02/2013 <--> 11/11/2018
-fev19ToNov10 = '/keno_gagnant_a_vie.csv'
-
-#16/09/1993 <--> 23/02/2013  
-mar17ToFev19 = '/keno.csv'
-
-# Les 3 derniers fichiers csv ne sont pas formatés de la meme façon , la colonne des resultats est L au lieu de M
-csv_files = [nov19ToNow, fev19ToNov10, mar17ToFev19]#, may76ToOct08]
-
-# Tableau qui sera comparé avec tous les autres tableaux suivant
-die_daySort = False
-lap_more = count_day = 0
-lap = 1
-limit_tirages = 12
+    print('\n Calcul en cours ...\n')
+#END init()
+#----------------------------------------------------------
 
 def compare():
     global count_day
     global limit_tirages
 
     for file_lap, document in enumerate(csv_files):
-        #print('A: {} B:{}'.format(file_lap,document))
-
         with open(path+document, 'r') as csv_file:
             csv_tirages = csv.reader(csv_file, delimiter=';')
             
             for row_lap, row in enumerate(csv_tirages):
-                #print('C: {} D:{}'.format(row_lap, row))
                 if (row_lap == 0):
                     continue
-                
+                #END if
                 sTirage = row[4:24]
                 sDate = row[1]
                 sDay_phase = row[2]
                 iTirage = map(int, sTirage)
 
-                if( count_day < limit_tirages ):
+                if(count_day < limit_tirages):
                     count_day += 1
                     somByMonth(sDate, sDay_phase, sTirage)
-                
-                topNumbers(sDate, sDay_phase, iTirage)
+                #END if
 
+                topNumbers(sDate, sDay_phase, iTirage)
+            #END for
+    #END for
 
     if(os.path.exists("./logkeno/stats-{}.txt".format(datetime.today().strftime("%d-%m-%Y")))):
         file = open("./logkeno/stats-{}.txt".format(datetime.today().strftime("%d-%m-%Y")),"w+")
-
     else:
         file = open("./logkeno/stats-{}.txt".format(datetime.today().strftime("%d-%m-%Y")),"a+")
 
-    sort_orders = sorted(tops_midi.items(), key=lambda x: x[1], reverse=True)
-    print('TOP Numeros pour ce MIDI')
-    file.write('TOP Numeros pour ce MIDI \n')  
-    for number in sort_orders:
-        print('N '+number[0], number[1])
-        file.write('N{}, {} \n'.format(number[0], number[1])) 
+    items_phases_day =  {'tops_midi':tops_midi.items(), 'tops_soir':tops_soir.items(), 'tops_midi_month':tops_midi_month.items(), 'tops_soir_month':tops_soir_month.items()}
 
-    sort_orders = sorted(tops_soir.items(), key=lambda y: y[1], reverse=True)
-    print('TOP Numeros pour ce SOIR')
-    file.write('TOP Numeros pour ce SOIR \n')  
-    for number in sort_orders:
-        print('N '+number[0], number[1])
-        file.write('N{}, {} \n'.format(number[0], number[1]))  
-    
-
-    sort_orders = sorted(tops_midi_month.items(), key=lambda z: z[1], reverse=True)
-    print('TOP Numeros sur les SIX DERNIERS tirages du MIDI')
-    file.write('TOP Numeros sur les SIX DERNIERS tirages du MIDI \n')  
-    for number in sort_orders:
-        print('N '+number[0], number[1])
-        file.write('N{}, {} \n'.format(number[0], number[1]))  
-    
-
-    sort_orders = sorted(tops_soir_month.items(), key=lambda a: a[1], reverse=True)
-    print('TOP Numeros sur les SIX DERNIERS tirages du SOIR')
-    file.write('TOP Numeros sur les SIX DERNIERS tirages du SOIR \n')  
-    for number in sort_orders:
-        print('N '+number[0], number[1])
-        file.write('N{}, {} \n'.format(number[0], number[1]))
+    for index, ph in enumerate(items_phases_day):
+        sort_orders = sorted(items_phases_day[ph], key=lambda x: x[1], reverse=True)
+        print('{} Numeros'.format(ph))
+        file.write('{} Numeros \n'.format(ph))  
+        for number in sort_orders:
+            print('N '+number[0], number[1])
+            file.write('N{}, {} \n'.format(number[0], number[1])) 
     file.close()
 
     #send stats in a telegram channel
     os.system('telegram-send --file ./logkeno/stats-{}.txt'.format(datetime.today().strftime("%d-%m-%Y")))
 
-"""askInit()"""
+#End def compare()
+#----------------------------------------------------------
 
-#nettoie le terminal
-os.system('clear')
+###------------------------------------------------------------- START HERE 
 
-print('\n Mise à jour des derniers tirages en cours ...\n')
-
-#download last result file and unzip
-os.system('wget https://media.fdj.fr/static/csv/keno/keno_201811.zip && unzip -o keno_201811.zip')
-#delete zip file
-os.system('rm -rf keno_201811.zip')
-#Move to keno file 
-os.system('mv keno_201811.csv keno')
-print('Done')
-
-print('\n Calcul en cours ...\n')
-
+# Step 1 : Launch init function 
+init()
+# Step 2 : Launch compare function 
 compare()
-#os.system('python match.py {}'.format())
