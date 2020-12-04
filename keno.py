6885ce49d028af7bac03d50fb27bffcea8af3ae8
root <root@vps-465c390c.vps.ovh.net>
2020-12-04 15:56:11 +0800

    ..

--- keno.py
+++ keno.py
@@ -67,7 +67,7 @@ def compare():
     file.close()
 
     #send stats in a telegram channel
-    os.system('telegram-send --file ./logkeno/stats-{}.txt'.format(datetime.today().strftime("%d-%m-%Y")))
+    #os.system('telegram-send --file ./logkeno/stats-{}.txt'.format(datetime.today().strftime("%d-%m-%Y")))
 
 #End def compare()
 #----------------------------------------------------------
