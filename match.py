4663bae0fce438d280efa8585ada9b9ad865a424
Lory LÉTICÉE <loryleticee@gmail.com>
2020-12-06 17:43:42 +0100

    init

--- match.py
+++ match.py
@@ -2,17 +2,19 @@ import os
 from datetime import datetime, timedelta, date
 from constant import tops_midi
 from utils import getDate, getDayWord, month_name, day_name
-isN1 = isN2 = isN3 = False
+isN1 = isN2 = isN3 = isN4 = isN5 = False
 
 fileIsEmpty = True
 
-def somByDay3(sTirage, n1, n2, n3, ip, sDate):
+def somByDay3(sTirage, n1, n2, n3, n4, n5, ip, sDate):
     ipAdress = ip.replace('.','')
     now =  date.today().strftime("%d-%m-%Y")
 
     global isN1
     global isN2
     global isN3
+    global isN4
+    global isN5
     global fileIsEmpty
 
     for item in sTirage:
@@ -28,7 +30,13 @@ def somByDay3(sTirage, n1, n2, n3, ip, sDate):
         if(str(item) == str(n3)):
             isN3 = True
 
-    if(isN1 == True & isN2 == True & isN3 == True):
+        if(str(item) == str(n4)):
+            isN4 = True
+
+        if(str(item) == str(n5)):
+            isN5 = True
+
+    if(isN1 == True & isN2 == True & isN3 == True & isN4 == True & isN5 == True):
         if(os.path.exists("./exist/exist-"+ now +'-'+ ipAdress+".txt")):
             fileTiragExist = open("./exist/exist-"+ now +'-'+ ipAdress+".txt","a+")
         else:
@@ -38,10 +46,10 @@ def somByDay3(sTirage, n1, n2, n3, ip, sDate):
         tirage = str(iTirage)
         fileTiragExist.write(str('[["'+getDate(sDate)+'"],'+str(tirage)+']'))
         fileIsEmpty = False
-        isN1 = isN2 = isN3 = False
+        isN1 = isN2 = isN3 = isN4 = isN5 = False
         print(tirage + ' ' + getDate(sDate))
     else:
-        isN1 = isN2 = isN3 = False
+        isN1 = isN2 = isN3 = isN4 = isN5 = False
     #fileTiragExist.close()
     if (fileIsEmpty):
         fileTiragExist = open("./exist/exist-"+ now +'-'+ ipAdress+".txt","w+")
